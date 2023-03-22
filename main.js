729b5463c4bafcdd5f4e657a48b6f48f1136d6fe
Daiana Paz <daianadenispaz@gmail.com>
2023-03-21 21:15:21 -0300

    primera parte

--- main.js
+++ main.js
@@ -1,21 +1,54 @@
 /*ENTRADAS AL CINE*/
 
-function saludar(){
-    alert("Bienvenido/a");
-}
-
-let edad;
-
-for(let i = 0; i < 10; i++){
-    saludar();
-    edad = prompt("Ingresar edad");
-    edad = parseInt(edad);
-    
-    if(edad > 17){
-        alert("Puede entrar");
-    }else{
-        alert("Debe entrar con un adulto");
+class Persona{
+    constructor(nombre, edad, tipoDeEntrada, formaDePago){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.tipoDeEntrada = tipoDeEntrada;
+        this.formaDePago = formaDePago;
     }
-
 }
-alert("No hay mas entradas");
+
+const personas = [];
+
+const formulario = document.getElementById('formulario');
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    agregarPersona();
+});
+
+function agregarPersona(){
+    const nombre = document.getElementById('nombre').value;
+    const edad = document.getElementById('edad').value;
+    const entrada = document.getElementById('entrada').value;
+    const pago = document.getElementById('pago').value;
+    const nuevaPersona = new Persona(nombre, edad, entrada, pago);
+    personas.push(nuevaPersona);
+    localStorage.setItem('personas', JSON.stringify(personas));
+    formulario.reset();
+}
+
+const usuarios = document.getElementById('usuarios');
+
+const verCarrito = document.getElementById('verCarrito');
+
+verCarrito.addEventListener('click', () => {
+    mostrarPersonas();
+});
+
+function mostrarPersonas(){
+    usuarios.innerHTML = '';
+    personas.forEach((persona) => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div>
+            <h2>Nombre: ${persona.nombre}</h2>
+            <p>Edad: ${persona.edad}</p>
+            <p>Tipo de entrada: ${persona.tipoDeEntrada}</p>
+            <p>Forma de pago: ${persona.formaDePago}</p>
+        </div>`;
+        div.className = "colorDeFondo";
+        usuarios.appendChild(div);
+    });
+}
